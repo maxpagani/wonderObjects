f50c724bb0c0a35b4e99069b6b834f5d1c27e99a
Massimiliano Pagani <massimiliano.pagani@schindler.com>
2024-05-13 14:10:42 +0200

    Add English localization

--- bin/makeExcuse.sh
+++ bin/makeExcuse.sh
@@ -5,9 +5,12 @@
 # @date 2020-02-07
 #
 
-articleFor()
+set -u
+
+italianArticleFor()
 {
-  thing="$1"
+  local thing="$1"
+
   case ${thing:0:2} in
     ps|pn|gn|[zy]?|s[bcdfghjklmnpqrstvwxyz])
       echo "lo "
@@ -21,7 +24,21 @@ articleFor()
   esac
 }
 
-excuses=(
+articleFor()
+{
+  local lang="$1"
+  local thing="$2"
+  case "$lang" in
+    it)
+      italianArticleFor "$thing"
+      ;;
+    en)
+      echo -n "the "
+      ;;
+  esac
+}
+
+excuses_it=(
   '$subject era controintuitivo'
   '$subject era fuori fase'
   '$subject era in congiuntura'
@@ -50,20 +67,58 @@ excuses=(
   'qualcuno ha spostato $subject'
 )
 
+excuses_en=(
+  '$subject was counter intuitive'
+  '$subject was not in phase'
+  '$subject was on conjucture'
+  '$subject was crashed'
+  '$subject was on maintenance'
+  '$subject was charging'
+  '$subject was overloaded'
+  '$subject was in an exotic quantum state'
+  '$subject was in an uncompatible quantum state'
+  '$subject was not available'
+  '$subject was flat'
+  '$subject couldnt be found'
+  '$subject was too difficult'
+  '$subject was out of reach'
+  '$subject had no $(determinedObject)'
+  '$subject was in use'
+  '$subject was not in an ergonomic position'
+  '$subject was not operational'
+  '$subject was not properly oriented'
+  '$subject was not working'
+  '$subject was about to explode'
+  'I had to wait that $subject moved to the right position' 
+  'I had to wait that $subject returned in phase' 
+  'I had to wait that $subject would reset' 
+  'I could not find $subject'
+  'Someone moved $subject'
+)
+
 function randomIndex()
 {
-  count=${#excuses[@]}
+  local arrayName="${2}_${1}"
+  eval "local count=\${#$arrayName[@]}"
   echo $(( $RANDOM % $count ))
 }
 
 determinedObject()
 {
-  object=$(wonderObject.sh)
-  article=$(articleFor "$object")
+  local lang="$1"
+  local object=$(wonderObject.sh)
+  local article=$(articleFor "$lang" "$object")
   echo "$article$object"
 }
 
-subject="$(determinedObject)"
-index=$(randomIndex)
-excuse=${excuses[$index]}
+lang=${LANG?-en_US.UTF-8}
+lang=${lang%_*}
+if [ "${lang%.*}" = "C" ]
+then
+  lang="en"
+fi
+
+subject="$(determinedObject "$lang")"
+index=$(randomIndex $lang excuses)
+eval "excuse=\"\${excuses_$lang[$index]}\""
 eval echo $excuse
--- bin/wonderObject.sh
+++ bin/wonderObject.sh
@@ -1,189 +1,408 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # @author Massimiliano Pagani <maxpag@gmail.com>
 
-MAX_RANDOM=32767
+set -u
 
-prefixes="
-areo
-bi
-co
-eco
-emi
-endo
-epi
-eso
-eta
-iper
-ipo
-iso
-kilo
-mega
-micro
-mono
-nano
-omo
-pico
-stato
-tera
-tri
-turbo
-"
+lang=${LANG?-en_US.UTF-8}
+lang=${lang%_*}
 
-names="\
-ammortizzatore
-abbattitore
-analizzatore
-attrattore
-catalizzatore
-catamorfimetro
-dematerializzatore
-fasimetro
-flareometro
-flussometro
-flussore
-flusso_canalizzatore
-funtometro
-funtore
-gaussimetro
-generatore
-giratore
-globulizzatore
-gradiente
-induttore
-iniettore
-materializzatore
-misuratore
-monoide
-parzializzatore
-polimerizzatore
-radimetro
-radiometro
-reattore
-repulsore
-rettificatore
-rifasatore
-rotore
-sequenziatore
-sfasatore
-sminuzzatore
-smussimetro
-"
+case "$lang" in
+  it|en)
+    ;;
+  C.UTF-8)
+    lang=en
+    ;;
+  *)
+    echo "Unsupported language ($LANG)"
+    exit 1
+    ;;
+esac
 
-objects="\
-atomico
-di_antimateria
-di_atomi
-di_campo
-di_dilitio
-di_ioni
-di_materia_esotica
-di_materia_oscura
-di_molecole
-di_onde_gravitazionali
-di_tachioni
-per_siluro
-"
+prefixes_it=(
+  "areo"
+  "bi"
+  "co"
+  "eco"
+  "emi"
+  "endo"
+  "epi"
+  "eso"
+  "eta"
+  "iper"
+  "ipo"
+  "iso"
+  "kilo"
+  "mega"
+  "micro"
+  "mono"
+  "nano"
+  "omo"
+  "pico"
+  "stato"
+  "tera"
+  "tri"
+  "turbo"
+)
 
-qualifiers="\
-a_inversione_di_fase
-abeliano
-adiabatico
-a_induzione
-alcaloide
-al_dilitio
-amorfo
-analitico
-analogico
-anamorfico
-a_onde_moventi
-a_sfasamento
-asimmetrico
-assiale
-atomico
-a_transizioni_quantiche
-cardinale
-catamorfico
-coassiale
-di_Cauchy
-di_Cochrane
-di_Einstein-Rosen
-di_Eulero
-differenziale
-digitale
-di_Pascal
-di_Tesla
-di_Turing
-di_Watt
-endotermico
-entropico
-enzimatico
-esotermico
-fasico
-fotonico
-genico
-gravitazionale
-igroscopico
-inerte
-inerziale
-integrale
-ionico
-iperbolico
-isotropico
-isocrono
-laser
-logaritmico
-logistico
-magnetico
-molecolare
-monadico
-nanotecnologico
-nucleare
-omeotropico
-omomorfico
-parabolico
-polimerico
-quantico
-radiante
-ricorsivo
-simmetrico
-solare
-tachionico
-telescopico
-trifasico
-trigonometrico
-universale
-"
+prefixes_en=(
+  "areo"
+  "bi"
+  "co"
+  "eco"
+  "emi"
+  "endo"
+  "epi"
+  "exo"
+  "eta"
+  "hyper"
+  "hypo"
+  "iso"
+  "kilo"
+  "mega"
+  "micro"
+  "mono"
+  "nano"
+  "homo"
+  "pico"
+  "stato"
+  "tera"
+  "tri"
+  "turbo"
+)
 
-function randomSelect()
+names_it=(
+  "ammortizzatore"
+  "abbattitore"
+  "analizzatore"
+  "attrattore"
+  "catalizzatore"
+  "catamorfimetro"
+  "dematerializzatore"
+  "fasimetro"
+  "flareometro"
+  "flussometro"
+  "flussore"
+  "flusso canalizzatore"
+  "funtometro"
+  "funtore"
+  "gaussimetro"
+  "generatore"
+  "giratore"
+  "globulizzatore"
+  "gradiente"
+  "induttore"
+  "iniettore"
+  "materializzatore"
+  "misuratore"
+  "monoide"
+  "parzializzatore"
+  "polimerizzatore"
+  "radimetro"
+  "radiometro"
+  "reattore"
+  "repulsore"
+  "rettificatore"
+  "rifasatore"
+  "rotore"
+  "sequenziatore"
+  "sfasatore"
+  "sminuzzatore"
+  "smussimetro"
+)
+
+names_en=(
+  "dumper"
+  "blast chiller"
+  "analyzer"
+  "attractor"
+  "catalyst"
+  "catamorphimeter"
+  "dematerializer"
+  "phase meter"
+  "flare meter"
+  "flux meter"
+  "fluxer"
+  "flux capacitor"
+  "functometer"
+  "functor"
+  "gauss meter"
+  "generator"
+  "gyrator"
+  "globulizer"
+  "gradient"
+  "inductor"
+  "injector"
+  "materializer"
+  "meter"
+  "monoid"
+  "divider"
+  "polymerizer"
+  "radimeter"
+  "radiometer"
+  "reactor"
+  "repulsor"
+  "rectifier"
+  "phase plug"
+  "rotor"
+  "sequencer"
+  "phase shifter"
+  "shredder"
+  "bevel gauge"
+)
+
+objects_it=(
+  "atomico"
+  "di antimateria"
+  "di atomi"
+  "di campo"
+  "di dilitio"
+  "di ioni"
+  "di materia esotica"
+  "di materia oscura"
+  "di molecole"
+  "di onde gravitazionali"
+  "di tachioni"
+  "per siluro"
+)
+
+objects_en=(
+  "atom"
+  "antimatter"
+  "field"
+  "dilithium"
+  "ion"
+  "exotic matter"
+  "dark matter"
+  "molecule"
+  "gravitational wave"
+  "tachyon"
+  "torpedo"
+)
+
+qualifiers_it=(
+  "a inversione di fase"
+  "abeliano"
+  "adiabatico"
+  "a induzione"
+  "alcaloide"
+  "al dilitio"
+  "amorfo"
+  "analitico"
+  "analogico"
+  "anamorfico"
+  "a onde moventi"
+  "a sfasamento"
+  "asimmetrico"
+  "assiale"
+  "atomico"
+  "a transizioni quantiche"
+  "cardinale"
+  "catamorfico"
+  "coassiale"
+  "di Cauchy"
+  "di Cochrane"
+  "di Einstein-Rosen"
+  "di Eulero"
+  "differenziale"
+  "digitale"
+  "di Pascal"
+  "di Tesla"
+  "di Turing"
+  "di Watt"
+  "endotermico"
+  "entropico"
+  "enzimatico"
+  "esotermico"
+  "fasico"
+  "fotonico"
+  "genico"
+  "gravitazionale"
+  "igroscopico"
+  "inerte"
+  "inerziale"
+  "integrale"
+  "ionico"
+  "iperbolico"
+  "isotropico"
+  "isocrono"
+  "laser"
+  "logaritmico"
+  "logistico"
+  "magnetico"
+  "molecolare"
+  "monadico"
+  "nanotecnologico"
+  "nucleare"
+  "omeotropico"
+  "omomorfico"
+  "parabolico"
+  "polimerico"
+  "quantico"
+  "radiante"
+  "ricorsivo"
+  "simmetrico"
+  "solare"
+  "tachionico"
+  "telescopico"
+  "trifasico"
+  "trigonometrico"
+  "universale"
+)
+
+qualifiers_en=(
+  "abelian"
+  "adiabatic"
+  "alkaloid"
+  "amorphous"
+  "analog"
+  "analytical"
+  "anamorphic"
+  "asymmetrical"
+  "atomic"
+  "axial"
+  "cardinal"
+  "catamorphic"
+  "Cauchy's"
+  "coaxial"
+  "Cochrane's"
+  "differential"
+  "digital"
+  "dilithium based"
+  "Einstein-Rosen"
+  "endothermic"
+  "entropic"
+  "enzymatic"
+  "Euler's"
+  "exothermic"
+  "genic"
+  "gravitational"
+  "homeotropic"
+  "homomorphic"
+  "hygroscopic"
+  "hyperbolic"
+  "induction"
+  "inert"
+  "inertial"
+  "ionic"
+  "isochronous"
+  "isotropic"
+  "laser"
+  "logarithmic"
+  "logistical"
+  "magnetic"
+  "molecular"
+  "monadic"
+  "nanotechnological"
+  "nuclear"
+  "parabolic"
+  "Pascal's"
+  "phase displacement"
+  "phase inversion"
+  "phasic"
+  "photonic"
+  "polymeric"
+  "quantum"
+  "quantum transition"
+  "radiant"
+  "recursive"
+  "solar"
+  "symmetrical"
+  "tachyonic"
+  "telescopic"
+  "Tesla's"
+  "trigonometric"
+  "triphasic"
+  "Turing's"
+  "universal"
+  "Watt's"
+  "wave motion"
+  "whole wheat"
+)
+
+drawRandom()
 {
-  count=$(echo "$1" | wc -w )
-  index=$(( 1+($RANDOM % $count) ))
-  echo $index | \
-    select name in $1; do echo ${name//_/ } ; break; done 2>/dev/null
+  local upper=$1
+  local random=$RANDOM
+  echo -n $(( $random % $upper ))
 }
 
-theName=$(randomSelect "$names" )
-theObject=$(randomSelect "$objects" )
-theQualifier=$(randomSelect "$qualifiers" )
+echoRandomEntry()
+{
+  local lang="$1"
+  local arrayName="$2_$lang"
+  eval "local count=\${#$arrayName[@]}"
+  local index=$(drawRandom $count)
+  eval "echo -n \"\${$arrayName[$index]}\""
+}
 
-random=$RANDOM
-if (( $random > $MAX_RANDOM/2 ))
-then
-  thePrefix=$(randomSelect "$prefixes")
-  echo -n "$thePrefix-"
-  if (( $random < $MAX_RANDOM/10 ))
+echoRandomPrefix()
+{
+  local lang=$1
+  echoRandomEntry $lang prefixes
+}
+
+drawPrefixes()
+{
+  local lang="$1"
+  local wantOnePrefix="$(drawRandom 2)"
+
+  if [ $wantOnePrefix = 0 ]
   then
-    otherPrefix=$(randomSelect "$prefixes")
-    if [ "$thePrefix" != "$otherPrefix" ]
+    echoRandomPrefix "$lang"
+    local wantTwoPrefixex="$(drawRandom 10)"
+    if [ $wantTwoPrefixex = 0 ]
     then
-      echo -n "$otherPrefix-"
+      echoRandomPrefix "$lang"
     fi
   fi
-fi
+}
 
-if (( $RANDOM > $MAX_RANDOM/3 ))
+print3()
+{
+  local lang="$1"
+  local prefixes="$2"
+  local name="$3"
+  local object="$4"
+  local qualifier="$5"
+  case "$lang" in
+    it)
+      echo "$prefixes$name" "$object" "$qualifier"
+      ;;
+    en)
+      echo "$qualifier" "$object" "$prefixes$name"
+      ;;
+  esac
+}
+
+print2()
+{
+  local lang="$1"
+
+  local prefixes="$2"
+  local name="$3"
+  local qualifier="$4"
+  case "$lang" in
+    it)
+      echo "$prefixes$name" "$qualifier"
+      ;;
+    en)
+      echo "$qualifier" "$prefixes$name"
+      ;;
+  esac
+}
+
+
+theName=$(echoRandomEntry "$lang" names )
+theObject=$(echoRandomEntry "$lang" objects )
+theQualifier=$(echoRandomEntry "$lang" qualifiers )
+
+
+prefixes="$(drawPrefixes "$lang")"
+
+wantObject=$(drawRandom 3)
+if [ $wantObject = 0 ]
 then
-  echo "$theName $theObject $theQualifier"
+  print3 "$lang" "$prefixes" "$theName" "$theObject" "$theQualifier"
 else
-  echo "$theName $theQualifier"
+  print2 "$lang" "$prefixes" "$theName" "$theQualifier"
 fi
